a3061396dd29a5cccde846c605acd60e56b161c3
fabs1902 <fabiolamartinez190201@gmail.com>
2024-08-31 19:09:36 -0600

    Procedimiento de guia

---
+++ APIGateway_MR200114/LibroAPI/Models/Libro.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LibroAPI.Models
+{
+    public class Libro
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Nombre { get; set; }
+
+        [Required]
+        public  string? Autor { get; set; }
+
+        public int? AnioPublicacion { get; set; }
+
+
+    }
+}
---
+++ APIGateway_MR200114/LibroAPI/Models/LibroContext.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace LibroAPI.Models
+{
+    public class LibroContext : DbContext
+    {
+        public LibroContext(DbContextOptions<LibroContext> options) : base(options) { }
+
+        public DbSet<Libro> Libros { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Libro>().HasData(
+                new Libro
+                {
+                    Id = 1,
+                    Nombre = "Cien años de soledad",
+                    Autor = "Gabriel García Márquez",
+                    AnioPublicacion = 1967
+                },
+                new Libro
+                {
+                    Id = 2,
+                    Nombre = "Don Quijote de la Mancha",
+                    Autor = "Don Quijote de la Mancha",
+                    AnioPublicacion = 1605
+                },
+                new Libro
+                {
+                    Id = 3,
+                    Nombre = "El amor en los tiempos del cólera",
+                    Autor = "Gabriel García Márquez",
+                    AnioPublicacion = 1985
+                }
+
+                );
+        }
+    }
+}
